11c47f79a0cff247b5e538ed0df2194187596d60
Marwa Mahmoud <marwa@bluskyint.com>
2024-10-17 17:59:38 +0300

    change side-nav

--- src/app/core/icons/navigation-icons/navigation-icon.component.ts
+++ src/app/core/icons/navigation-icons/navigation-icon.component.ts
@@ -1,9 +1,15 @@
-import {
- Component, Input } from '@angular/core';
+import { NgStyle } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, Input } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-navigation-icon',
-  template: `<img [src]="iconPath" alt="{{ iconName }} icon" class="w-6 h-6" />`,
+  imports: [NgStyle],
+  template: ` <div
+    [innerHTML]="sanitizedSvgContent"
+    [ngStyle]="{ color: iconColor }"
+  ></div>`,
   styles: [
     `
       svg {
@@ -15,7 +21,31 @@ import {
 })
 export class NavigationIconComponent {
   @Input() iconName!: string;
+  @Input() iconColor: string = 'currentColor';
+  sanitizedSvgContent!: SafeHtml;
+  constructor(private http: HttpClient, private sanitizer: DomSanitizer) {}
+  ngOnInit(): void {
+    this.loadSvg();
+  }
+  loadSvg() {
+    const svgPath = `assets/icons/navigation/${this.iconName}.svg`;
 
+    this.http.get(svgPath, { responseType: 'text' }).subscribe(
+      (svgContent) => {
+        // Replace the 'fill' attribute with 'currentColor' to allow CSS color control
+        const updatedSvg = svgContent.replace(
+          /fill="[^"]*"/g,
+          'fill="currentColor"'
+        );
+        // Sanitize and assign the SVG content to be displayed
+        this.sanitizedSvgContent =
+          this.sanitizer.bypassSecurityTrustHtml(updatedSvg);
+      },
+      (error) => {
+        console.error('Error loading SVG:', error);
+      }
+    );
+  }
   get iconPath(): string {
     return `assets/icons/navigation/${this.iconName}.svg`;
   }
