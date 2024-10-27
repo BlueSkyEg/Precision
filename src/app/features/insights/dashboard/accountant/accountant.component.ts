01ae795612c69ecf8f55a281538deae9b4712df5
Marwa Mahmoud <marwa@bluskyint.com>
2024-10-27 16:59:48 +0300

    sorting

--- src/app/features/insights/dashboard/accountant/accountant.component.ts
+++ src/app/features/insights/dashboard/accountant/accountant.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { DashboardService } from 'app/core/services/dashboard/dashboard.service';
 import { TableComponent } from 'app/shared/components/table/table.component';
 import { TopNavComponent } from 'app/shared/components/top-nav/top-nav.component';
 import { TotalTransactionsCardComponent } from 'app/shared/components/total-transactions-card/total-transactions-card.component';
+import { ITab } from 'app/shared/interfaces/insights/itab';
+import { IUser } from 'app/shared/interfaces/insights/iuser';
 
 @Component({
   selector: 'app-accountant',
@@ -10,6 +13,12 @@ import { TotalTransactionsCardComponent } from 'app/shared/components/total-tran
   templateUrl: './accountant.component.html',
 })
 export class AccountantComponent {
+  _DashboardService = inject(DashboardService);
+
+  selectedTabIndex = 0;
+  pending = 0;
+  awaiting = 0;
+  reviewed = 0;
   breadcrumbItems: any[] = [
     { label: 'Insights', routerLink: '/insights' },
     { label: 'Dashboard', routerLink: '/insights/dashboard' },
@@ -18,4 +27,42 @@ export class AccountantComponent {
       routerLink: '/insights/dashboard/accountant-dashboard',
     },
   ];
+  tabs: ITab[] = [
+    { title: 'My Profiles', searchTitle: 'Accountant', count: 0, users: [] },
+  ];
+
+  ngOnInit() {
+    this.getAccountants();
+  }
+
+  getAccountants() {
+    this._DashboardService.getProfiles().subscribe({
+      next: (response) => {
+        this.tabs[0].users = response.data.items;
+        this.tabs[0].count = this.tabs[0].users.length;
+        if (this.selectedTabIndex === 0)
+          this.updateTransactionCounts(this.tabs[0].users);
+      },
+    });
+  }
+  updateTransactionCounts(users: IUser[]) {
+    this.pending = users.reduce(
+      (total, user) => total + user.pendingTrxCount,
+      0
+    );
+    this.awaiting = users.reduce(
+      (total, user) => total + user.awaitingTrxCount,
+      0
+    );
+    this.reviewed = users.reduce(
+      (total, user) => total + user.reviewedTrxCount,
+      0
+    );
+  }
+
+  onTabChange(index: number) {
+    this.selectedTabIndex = index;
+    const selectedUsers = this.tabs[index].users;
+    this.updateTransactionCounts(selectedUsers);
+  }
 }
--- src/app/features/insights/dashboard/admin/admin.component.ts
+++ src/app/features/insights/dashboard/admin/admin.component.ts
@@ -1,18 +1,94 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { DashboardService } from 'app/core/services/dashboard/dashboard.service';
 import { TableComponent } from 'app/shared/components/table/table.component';
 import { TopNavComponent } from 'app/shared/components/top-nav/top-nav.component';
 import { TotalTransactionsCardComponent } from 'app/shared/components/total-transactions-card/total-transactions-card.component';
+import { ITab } from 'app/shared/interfaces/insights/itab';
+import { IUser } from 'app/shared/interfaces/insights/iuser';
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [TopNavComponent,TotalTransactionsCardComponent,TableComponent],
+  imports: [TopNavComponent, TotalTransactionsCardComponent, TableComponent],
   templateUrl: './admin.component.html',
 })
-export class AdminComponent {
+export class AdminComponent implements OnInit {
+  _DashboardService = inject(DashboardService);
+
+  selectedTabIndex = 0;
+  pending = 0;
+  awaiting = 0;
+  reviewed = 0;
+
   breadcrumbItems: any[] = [
     { label: 'Insights', routerLink: '/insights' },
     { label: 'Dashboard', routerLink: '/insights/dashboard' },
     { label: 'Admin', routerLink: '/insights/dashboard/admin-dashboard' },
   ];
+
+  tabs: ITab[] = [
+    { title: 'Accountants', count: 0, users: [] },
+    { title: 'Clients', count: 0, users: [] },
+    { title: 'Businesses', count: 0, users: [] },
+  ];
+
+  ngOnInit() {
+    this.getAccountants();
+    this.getClients();
+    this.getBusinesses();
+  }
+
+  getAccountants() {
+    this._DashboardService.getProfiles().subscribe({
+      next: (response) => {
+        this.tabs[0].users = response.data.items;
+        this.tabs[0].count = this.tabs[0].users.length;
+        if (this.selectedTabIndex === 0)
+          this.updateTransactionCounts(this.tabs[0].users);
+      },
+    });
+  }
+
+  getClients() {
+    this._DashboardService.getClients().subscribe({
+      next: (response) => {
+        this.tabs[1].users = response.data.items;
+        this.tabs[1].count = this.tabs[1].users.length;
+        if (this.selectedTabIndex === 1)
+          this.updateTransactionCounts(this.tabs[1].users);
+      },
+    });
+  }
+
+  getBusinesses() {
+    this._DashboardService.getbusinesses().subscribe({
+      next: (response) => {
+        this.tabs[2].users = response.data.items;
+        this.tabs[2].count = this.tabs[2].users.length;
+        if (this.selectedTabIndex === 2)
+          this.updateTransactionCounts(this.tabs[2].users);
+      },
+    });
+  }
+
+  updateTransactionCounts(users: IUser[]) {
+    this.pending = users.reduce(
+      (total, user) => total + user.pendingTrxCount,
+      0
+    );
+    this.awaiting = users.reduce(
+      (total, user) => total + user.awaitingTrxCount,
+      0
+    );
+    this.reviewed = users.reduce(
+      (total, user) => total + user.reviewedTrxCount,
+      0
+    );
+  }
+
+  onTabChange(index: number) {
+    this.selectedTabIndex = index;
+    const selectedUsers = this.tabs[index].users;
+    this.updateTransactionCounts(selectedUsers);
+  }
 }
