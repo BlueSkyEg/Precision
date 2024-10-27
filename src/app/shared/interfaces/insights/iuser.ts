691800d8ec758ae831253a7331e1bedf49c032fe
Marwa Mahmoud <marwa@bluskyint.com>
2024-10-27 16:58:45 +0300

    sorting

---
+++ src/app/shared/interfaces/insights/iuser.ts
@@ -0,0 +1,8 @@
+export interface IUser {
+  qbMemberId: string;
+  qbMemberName: string;
+  companiesCount: number;
+  pendingTrxCount: number;
+  awaitingTrxCount: number;
+  reviewedTrxCount: number;
+}
